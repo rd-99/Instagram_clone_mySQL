c67dcf1e179d53ca86b9c6a0c4b09eace30a9dcc
rd-99 <63721098+rd-99@users.noreply.github.com>
2021-05-25 17:15:21 +0530

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users(
+    id Integer AUTO_INCREMENT PRIMARY KEY,
+    username Varchar (255) Unique Not null,
+    created_at Timestamp DEFAULT now()
+);
+
+create TABLE photos(
+
+    id integer AUTO_INCREMENT PRIMARY KEY,
+    image_url varchar(255) not null,
+    user_id integer not null,
+    created_at Timestamp DEFAULT now(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    
+);
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    photo_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
